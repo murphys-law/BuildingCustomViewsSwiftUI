bb1b9a4f4c47b339e7d43901c905981415bb4e57
Claire <cmurphy@squareup.com>
2019-06-08 10:45:39 -0700

    Initial Commit

---
+++ BuildingCustomViewsSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BuildingCustomViewsSwiftUI
+//
+//  Created by Claire Murphy on 6/8/19.
+//  Copyright © 2019 Claire Murphy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
